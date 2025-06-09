617891cc40ae0241492462b4346bc7e284643270
ManinderSingh4000 <saabm314@gmail.com>
2025-06-09 13:16:49 +0530

    Addded The Factorial Code

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-import numpy as np
-def generate_random_array(size):
-    return np.random.rand(size)
+## Find Factorial of a Number
+import time
+def fact(n ,d):
+    if n in d:
+        return d[n]
+    else:
+        d[n]= n * fact(n-1 , d)
+    return d[n] 
 
-def calculate_mean(array):
-    return np.mean(array)   
-
-print(calculate_mean(generate_random_array(10)))
+d= {0: 1, 1: 1} 
+start = time.time()  # Start time measurement
+factorial = fact(15, d)
+time_taken = time.time()  # End time measurement
+print(f"Time taken: {time_taken - start} seconds")
+print(factorial)  

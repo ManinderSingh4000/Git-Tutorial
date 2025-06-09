482021957f7e4d06a9b39e228971e438677c53c0
ManinderSingh4000 <saabm314@gmail.com>
2025-06-09 15:37:09 +0530

    Git Commands added Successfully...

---
+++ Git_Commands.py
@@ -0,0 +1,19 @@
+"""
+
+# git branch -> used to List the branches in your repository.
+
+# git branch -a -> List all branches (local and remote).
+
+# git checkout <branch_name> -> Switch to the specified branch.
+
+# git checkout -b <branch_name> -> Create a new branch.
+
+# git branch -d <branch_name> -> Delete the specified branch.
+
+# git branch -D <branch_name> -> Force delete the specified branch.  
+
+
+# git merge <branch_name> -> Merge the specified branch into the current branch.
+
+
+"""
